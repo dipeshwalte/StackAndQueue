1a3074da8bc00b33a7784d6da3f7a6607b2aed3f
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-14 14:53:36 +0530

    Add project files.

---
+++ StackAndQueue/LinkedListStack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    class LinkedListStack
+    {
+        Node top;
+        public void Push(int newData)
+        {
+            Node newNode = new Node(newData);
+            if (this.top != null)
+            {
+                newNode.next = this.top;
+            }
+            this.top = newNode;
+            Console.WriteLine($"New Node {newData} is added to the top");
+        }
+
+        public void Display()
+        {
+            Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("No values to display");
+                return;
+            }
+
+        }
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("No element is present to pop");
+                return;
+            }
+            Console.WriteLine("The element to be popped is " + top.data);
+
+        }
+
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("No element is present to pop");
+                return;
+            }
+        }
+
+        public void IsEmpty()
+        {
+
+
+
+        }
+    }
+}
---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1
+
+        }
+    }
+}
